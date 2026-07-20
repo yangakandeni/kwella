e92e6bd25edc9931f688a151a465e6a78d9fe4df
yangakandeni <kandeni.yanga@gmail.com>
2026-07-20 02:17:20 +0200

    Remove kwella_rider_app and associated test files
    - Deleted pubspec.yaml to remove the Flutter project configuration for kwella_rider_app.
    - Removed bidding_provider_test.dart, driver_tracking_provider_test.dart, kwella_core_import_test.dart, and widget_test.dart to eliminate unit tests related to the kwella_rider_app.

--- kwella-apps/kwella_driver_app/android/app/src/main/kotlin/com/kwella/driver/kwella_driver_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.kwella.driver.kwella_driver_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- kwella-apps/kwella_rider_app/android/app/src/main/kotlin/com/kwella/rider/kwella_rider_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.kwella.rider.kwella_rider_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
